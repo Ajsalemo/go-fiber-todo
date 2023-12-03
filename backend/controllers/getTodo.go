48fbd70760498b1df0635e3fc004ffdb0f0848fb
Anthony Salemo <ajssalemo@gmail.com>
2023-12-03 12:31:32 -0500

    adding better logic to deletes and error handling

--- backend/controllers/getTodo.go
+++ backend/controllers/getTodo.go
@@ -33,12 +33,23 @@ func GetTodo(cxt *fiber.Ctx) error {
 		}
 
 		db.Find(&tasks, parsedId)
-		// For now, marshal the return tasks struct into JSON and parse into a sring
-		out, err := json.Marshal(&tasks)
-		zap.L().Info(string(out))
-
-		// No tasks found is returned as an empty array
-		// Let the client-side handle displaying of no tasks, if so
-		return cxt.JSON(fiber.Map{"msg": &tasks})
+		if len(tasks) > 0 {
+			// For now, marshal the return tasks struct into JSON and parse into a sring
+			out, err := json.Marshal(&tasks)
+			if err != nil {
+				zap.L().Error(err.Error())
+			}
+			zap.L().Info("Found task with id: " + id)
+			zap.L().Info(string(out))
+			// No tasks found is returned as an empty array
+			// Let the client-side handle displaying of no tasks, if so
+			return cxt.JSON(fiber.Map{"msg": &tasks})
+		} else {
+			// No tasks found is returned as a 404 back to the client
+			// Let the client-side handle displaying of no tasks, if so
+			zap.L().Info("No task found with id: " + id)
+			// Send a HTTP 404 back since nothing was found with this id
+			return cxt.SendStatus(404)
+		}
 	}
 }

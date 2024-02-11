762375fb2dcef2f580c3f7722168bb7d9244acc4
Anthony Salemo <ajssalemo@gmail.com>
2024-02-11 15:40:50 -0500

    setting up useEffect()

--- frontend/react-frontend/src/components/dashboard/dashboard.js
+++ frontend/react-frontend/src/components/dashboard/dashboard.js
@@ -1,20 +1,27 @@
+import { useEffect } from "react";
 import { Footer } from "../footer/footer";
 import { Navbar } from "../navbar/navbar";
 import { Sidebar } from "../sidebar/sidebar";
 import { Task } from "../task/task";
 
-export const Dashboard = () => (
-    <div className="h-screen bg-cyan-500">
-        <Navbar />
-        <div className="h-full flex">
-            <div className="w-full flex">
-                <Sidebar />
-                <div className="w-full mt-20">
-                    <Task />
-                    <Task />
+export const Dashboard = () => {
+    useEffect(() => {
+        
+    })
+
+    return (
+        < div className="h-screen bg-cyan-500" >
+            <Navbar />
+            <div className="h-full flex">
+                <div className="w-full flex">
+                    <Sidebar />
+                    <div className="w-full mt-20">
+                        <Task />
+                        <Task />
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer />
-    </div>
-)
+            <Footer />
+        </div >
+    )
+}

27375e79685776457c47f85450a9faae4353cc4f
Anthony Salemo <ajssalemo@gmail.com>
2024-03-10 18:05:00 -0400

    adding a component for axios error handling

--- frontend/react-frontend/src/components/dashboard/dashboard.js
+++ frontend/react-frontend/src/components/dashboard/dashboard.js
@@ -1,13 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "../footer/footer";
 import { Navbar } from "../navbar/navbar";
 import { Sidebar } from "../sidebar/sidebar";
 import { Task } from "../task/task";
 
+import axios from "axios"
+import { Error } from "../error/error";
+
 export const Dashboard = () => {
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
     useEffect(() => {
-        
-    })
+        const getAllTodosOnLoad = async () => {
+            try {
+                setIsError(false)
+                const { data } = await axios.get("http://localhost:3000/api/todo/get")
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+                console.log(error.message);
+                setIsError(true)
+                setErrorMessage(error.message);
+            }
+        }
+
+        getAllTodosOnLoad()
+    }, [])
 
     return (
         < div className="h-screen bg-cyan-500" >
@@ -16,8 +35,14 @@ export const Dashboard = () => {
                 <div className="w-full flex">
                     <Sidebar />
                     <div className="w-full mt-20">
-                        <Task />
-                        <Task />
+                        {isError ? (
+                            <Error error={errorMessage} />
+                        ) : (
+                            <>
+                                <Task />
+                                <Task />
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
---
+++ frontend/react-frontend/src/components/error/error.js
@@ -0,0 +1,7 @@
+export const Error = ({ error }) => {
+    return (
+        <div>
+            <p>An error occured while loading tasks | error: {error}</p>
+        </div>
+    )
+}
